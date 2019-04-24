fee0ff804b06e7520ffd73ae687f1238f7a026e0
Jose <jgonzalezmn@gmail.com>
2019-04-23 21:33:31 -0400

    Final Project

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -10,11 +10,24 @@
   
 /*
   === GameObject ===
-  * createdAt
+  * createdAt   
   * name
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
+
+function GameObject(attributes){
+  this.createdAt = attributes.createdAt;
+  this.name = attributes.name;
+  this.dimensions = attributes.dimensions;
+}
+
+GameObject.prototype.destroy = function(){
+  return `${this.name} was removed from the game.`
+}
+
+
+
 
 /*
   === CharacterStats ===
@@ -22,6 +35,20 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function CharacterStats(childA){
+  GameObject.call(this, childA); // binding 
+  this.healthPoints = childA.healthPoints;
+}
+
+CharacterStats.prototype = Object.create(GameObject.prototype);  //binding for prototype methods
+
+CharacterStats.prototype.takeDamage = function(){
+  return `${this.name} took damage`
+}
+
+
+
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -33,15 +60,36 @@
   * should inherit takeDamage() from CharacterStats
 */
  
-/*
-  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
-  * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
-  * Instances of CharacterStats should have all of the same properties as GameObject.
-*/
 
-// Test you work by un-commenting these 3 objects and the list of console logs below:
+function Humanoid(ha){
+  CharacterStats.call(this, ha) //binding  "this" to character stats
+  this.team = ha.team;
+  this.weapons = ha.weapons;
+  this.language = ha.language;
+}
 
-/*
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+Humanoid.prototype.greet = function(){
+  return `${this.name} offers a greeting in ${this.language}`
+}
+
+
+
+
+
+
+
+
+// /*
+//   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
+//   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
+//   * Instances of CharacterStats should have all of the same properties as GameObject.
+// */
+
+// // Test you work by un-commenting these 3 objects and the list of console logs below:
+
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +150,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. When in the global scope, the value of “this” will be the window/console Object //the value of "this" will be the console object when it is on the global scope
+* 2. Whenever a function is called by a preceding dot, the object before that dot is this. //
+* 3. Whenever a constructor function is used, this refers to the specific instance of the object that is created and returned by the constructor function. // when using a constructor function "this" is specific to that oject which was created are returned.
+* 4. Whenever JavaScript’s call or apply method is used, this is explicitly defined.// 
 *
 * write out a code example of each explanation above
 */
@@ -13,14 +13,50 @@
 
 // code example for Window Binding
 
+function sayName(name) {
+    console.log(this);
+    return name;
+  }
+  sayName("MJ");
+
+
 // Principle 2
 
 // code example for Implicit Binding
 
+var me = {
+name : "Jose",
+age : 22,
+    sayName(){
+        console.log(this.name)
+    }
+};
+
 // Principle 3
 
 // code example for New Binding
+ 
+var Cars = function(make,model,type){
+    this.make = make;
+    this.model = model;
+    this.type = type;
+}
+
+var Audi = new Cars ("Audi", "S4", "Coupe");
+
+console.log(Audi);
 
 // Principle 4
 
 // code example for Explicit Binding
+
+var sayName = function(){
+    console.log("My name is " + this.name );
+};
+
+var John = {
+    name: "John",
+    age : 42
+};
+
+sayName.call(John);
